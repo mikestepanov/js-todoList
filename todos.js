11a73becb30cd94366408a41a14db86cb4465ac9
mikestepanov <mikestepanov777@gmail.com>
2017-11-08 03:42:15 -0800

    commit

--- todos.js
+++ todos.js
@@ -16,5 +16,24 @@ var todosList = {
       completed: false
     });
     this.displayTodos();
+  },
+  changeTodos: function(newEl, index) {
+    this.todos[index].todoText = newEl;
+    this.displayTodos();
+  },
+  deleteTodos: function(index) {
+    this.todos.splice(index, 1);
+    this.displayTodos();
+  },
+  toggleCompleted: function(index) {
+    var todo = this.todos[index];
+    todo.completed = !todo.completed;
+    this.displayTodos();
   }
 }
+
+
+todosList.displayTodos();
+todosList.addTodos('item 6');
+todosList.changeTodos('item 4', 0);
+todosList.toggleCompleted(0);
