94667a2c601b5a2fab6ab0889c6263bf009ed6fc
mikestepanov <mikestepanov777@gmail.com>
2017-11-11 06:14:01 -0800

    commit

--- todos.js
+++ todos.js
@@ -55,18 +55,11 @@ var todoList = {
   }
 }
 
-
-var displayTodosButton = document.getElementById('displayTodosButton');
-var toggleAllButton = document.getElementById('toggleAllButton');
-
-
-console.log(displayTodosButton);
-console.log(toggleAllButton);
-
-displayTodosButton.addEventListener('click', function() {
-  todoList.displayTodos();
-});
-
-toggleAllButton.addEventListener('click', function() {
-  todoList.toggleAll();
-});
+var handlers = {
+  displayTodos: function() {
+    todoList.displayTodos();
+  },
+  toggleAll: function() {
+    todoList.toggleAll();
+  }
+}
